07772cbacbe9a6e10bb29f4934f6f899e04d84b7
warappa <spoiltnr1@gmx.at>
2026-08-14 01:45:24 +0200

    Single-line statements, not pattern,...

--- Jither.DebugAdapter/Adapter.cs
+++ Jither.DebugAdapter/Adapter.cs
@@ -38,21 +38,51 @@ public abstract class Adapter
     {
         switch (request.UntypedArguments)
         {
-            case AttachArguments args: await AttachRequest(args); return null;
-            case CancelArguments args: await CancelRequest(args); return null;
-            case ConfigurationDoneArguments: await ConfigurationDoneRequest(); return null;
-            case DisconnectArguments args: await DisconnectRequest(args); return null;
-            case GotoArguments args: await GotoRequest(args); return null;
-            case LaunchArguments args: await LaunchRequest(args); return null;
-            case NextArguments args: await NextRequest(args); return null;
-            case PauseArguments args: await PauseRequest(args); return null;
-            case RestartArguments args: await RestartRequest(args); return null;
-            case RestartFrameArguments args: await RestartFrameRequest(args); return null;
-            case ReverseContinueArguments args: await ReverseContinueRequest(args); return null;
-            case StepBackArguments args: await StepBackRequest(args); return null;
-            case StepInArguments args: await StepInRequest(args); return null;
-            case StepOutArguments args: await StepOutRequest(args); return null;
-            case TerminateArguments args: await TerminateRequest(args); return null;
+            case AttachArguments args:
+                await AttachRequest(args);
+                return null;
+            case CancelArguments args:
+                await CancelRequest(args);
+                return null;
+            case ConfigurationDoneArguments:
+                await ConfigurationDoneRequest();
+                return null;
+            case DisconnectArguments args:
+                await DisconnectRequest(args);
+                return null;
+            case GotoArguments args:
+                await GotoRequest(args);
+                return null;
+            case LaunchArguments args:
+                await LaunchRequest(args);
+                return null;
+            case NextArguments args:
+                await NextRequest(args);
+                return null;
+            case PauseArguments args:
+                await PauseRequest(args);
+                return null;
+            case RestartArguments args:
+                await RestartRequest(args);
+                return null;
+            case RestartFrameArguments args:
+                await RestartFrameRequest(args);
+                return null;
+            case ReverseContinueArguments args:
+                await ReverseContinueRequest(args);
+                return null;
+            case StepBackArguments args:
+                await StepBackRequest(args);
+                return null;
+            case StepInArguments args:
+                await StepInRequest(args);
+                return null;
+            case StepOutArguments args:
+                await StepOutRequest(args);
+                return null;
+            case TerminateArguments args:
+                await TerminateRequest(args);
+                return null;
         }
 
         return request.UntypedArguments switch
