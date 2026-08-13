f8d19716fc7fc5e06853e132eee06acfd38934d7
warappa <spoiltnr1@gmx.at>
2026-08-14 01:09:20 +0200

    Migrate to slnx, use top level statement program

--- Jint.DebugAdapterExample/Program.cs
+++ Jint.DebugAdapterExample/Program.cs
@@ -1,92 +1,86 @@
 ﻿using Jint.DebugAdapter;
+using Jint.DebugAdapterExample;
 using Jither.DebugAdapter;
 using Jither.DebugAdapter.Helpers;
 
-namespace Jint.DebugAdapterExample
+Dictionary<string, Action<Options>> DemosById = new()
 {
-    public class Program
+    ["files"] = DemoFiles,
+    ["internal"] = DemoInternal,
+    ["running"] = DemoRunning
+};
+
+LogManager.Level = LogLevel.Verbose;
+LogManager.Provider = new ConsoleLogProvider();
+
+var options = new Options(args, DemosById);
+var demo = DemosById[options.DemoId];
+
+demo(options);
+
+return 0;
+
+static Endpoint CreateEndpoint(string? endpoint)
+{
+    if (endpoint == null)
     {
-        private static Dictionary<string, Action<Options>> DemosById = new()
-        {
-            ["files"] = DemoFiles,
-            ["internal"] = DemoInternal,
-            ["running"] = DemoRunning
-        };
+        return new StdInOutEndpoint();
+    }
+    if (Int32.TryParse(endpoint, out int port))
+    {
+        return new TcpEndpoint(port);
+    }
+    return new NamedPipeEndpoint(endpoint);
+}
 
-        private class Options
-        {
-            public string DemoId { get; } = "files";
-            public string? Endpoint { get; }
+static void DemoFiles(Options options)
+{
+    var endpoint = CreateEndpoint(options.Endpoint);
 
-            public Options(string[] args)
+    var host = new FilesScriptHost();
+    var adapter = new JintAdapter(host, host.Engine, endpoint);
+    host.RegisterConsole(adapter.Console);
+
+    adapter.StartListening();
+}
+
+static void DemoInternal(Options options)
+{
+    var endpoint = CreateEndpoint(options.Endpoint);
+
+    var host = new InternalScriptHost();
+    var adapter = new JintAdapter(host, host.Engine, endpoint);
+    host.RegisterConsole(adapter.Console);
+
+    adapter.StartListening();
+}
+
+static void DemoRunning(Options options)
+{
+    var endpoint = CreateEndpoint(options.Endpoint);
+
+    var host = new RunningScriptHost();
+    var adapter = new JintAdapter(host, host.Engine, endpoint);
+    host.RegisterConsole(adapter.Console);
+
+    adapter.Launch("scripts/index.js");
+}
+
+public class Options
+{
+    public string DemoId { get; } = "files";
+    public string? Endpoint { get; }
+
+    public Options(string[] args, Dictionary<string, Action<Options>> DemosById)
+    {
+        foreach (var arg in args)
+        {
+            if (DemosById.ContainsKey(arg))
             {
-                foreach (var arg in args)
-                {
-                    if (DemosById.ContainsKey(arg))
-                    {
-                        DemoId = arg;
-                        continue;
-                    }
-                    Endpoint = arg;
-                }
+                DemoId = arg;
+                continue;
             }
-        }
-
-        public static void Main(string[] args)
-        {
-            LogManager.Level = LogLevel.Verbose;
-            LogManager.Provider = new ConsoleLogProvider();
-
-            var options = new Options(args);
-            var demo = DemosById[options.DemoId];
-
-            demo(options);
-        }
-
-        private static Endpoint CreateEndpoint(string? endpoint)
-        {
-            if (endpoint == null)
-            {
-                return new StdInOutEndpoint();
-            }
-            if (Int32.TryParse(endpoint, out int port))
-            {
-                return new TcpEndpoint(port);
-            }
-            return new NamedPipeEndpoint(endpoint);
-        }
-
-        private static void DemoFiles(Options options)
-        {
-            var endpoint = CreateEndpoint(options.Endpoint);
-
-            var host = new FilesScriptHost();
-            var adapter = new JintAdapter(host, host.Engine, endpoint);
-            host.RegisterConsole(adapter.Console);
-
-            adapter.StartListening();
-        }
-
-        private static void DemoInternal(Options options)
-        {
-            var endpoint = CreateEndpoint(options.Endpoint);
-
-            var host = new InternalScriptHost();
-            var adapter = new JintAdapter(host, host.Engine, endpoint);
-            host.RegisterConsole(adapter.Console);
-
-            adapter.StartListening();
-        }
-
-        private static void DemoRunning(Options options)
-        {
-            var endpoint = CreateEndpoint(options.Endpoint);
-
-            var host = new RunningScriptHost();
-            var adapter = new JintAdapter(host, host.Engine, endpoint);
-            host.RegisterConsole(adapter.Console);
-
-            adapter.Launch("scripts/index.js");
+            Endpoint = arg;
         }
     }
 }
